6f2ae03a19aad39e2cef3a91e2f2b1b98e3a62ca
Tarun S Maidur <tarunmaidur@gmail.com>
2024-03-13 10:41:18 +0530

    apis

--- apiPractice/src/App.jsx
+++ apiPractice/src/App.jsx
@@ -1,47 +1,31 @@
-import axios from "axios";
 
 import React, { useEffect, useState } from "react";
+import { Link,Routes,Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Products from "./components/Products";
 
 const App = () => {
-  const [products, setProducts] = useState([]);
-
-  const getProducts = async () => {
-    const api = "https://fakestoreapi.com/products";
-
-    await axios.get(api).then((response) => {
-      console.log(response.data);
-      setProducts(response.data);
-    });
-  };
-  const addProduct = async () => {
-    const api = "https://fakestoreapi.com/products";
-
-    await axios
-      .post(api, {
-        title: "test product",
-        price: 13.5,
-        description: "lorem ipsum set",
-        image: "https://i.pravatar.cc",
-        category: "electronic",
-      })
-      .then((response) => {
-        console.log(response.data);
-      });
-  };
-
-  useEffect(() => {
-    getProducts();
-  }, []);
+  
   return (
-    <div id="cards">
-      {products.map((product) => (
-        <div id="card" key={product.id}>
-          <img src={product.image} alt={product.title} />
-          <h3>{product.title}</h3>
-          <p>{product.price}</p>
-        </div>
-      ))}
-    </div>
+    <>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+      <Link to="/products">Products</Link>
+    </nav>
+
+    <Routes>
+       
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/products" element={<Products />} />
+    </Routes>
+    </>
+    
   );
 };
 
